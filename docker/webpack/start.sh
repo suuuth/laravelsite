234a831c7a182729dc0186f5d4433c33b375208a
suuuth <kevin.sutherl@gmail.com>
2023-10-01 14:19:49 -0400

    isolating js/css

--- docker/webpack/start.sh
+++ docker/webpack/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "running"
-until cd /webpack && npm install
+until cd /resources && npm install
 do
     echo "Retrying npm install" >> log.txt
 done
-cd /webpack && npm run watch
+cd /resources && npm run watch
 tail -f /dev/null
